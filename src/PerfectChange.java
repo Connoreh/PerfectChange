17510413beeb99c47916a43c93eff2d6aea8dd8b
Connor Hellman <108197430+Connoreh@users.noreply.github.com>
2025-03-11 21:53:06 -0500

    Finish adding makeChangeDiv() and perfectChange()

--- src/PerfectChange.java
+++ src/PerfectChange.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class PerfectChange {
     static Scanner sc = new Scanner(System.in);
     static int qoh, doh, noh, poh; // Quarters.dimes,nickels,pennies on hand
+//    static int c = 0;
 
 
     public static void main(String[] args) {
+        int c = 0;
         String menu;
         int userChoice;
         boolean usingSet;
@@ -30,7 +32,8 @@ public class PerfectChange {
                     perfectChange(userChoice);
                 }
                 else {
-                    makeChange(userChoice);
+//                    makeChange(userChoice);
+                    makeChangeDiv(userChoice);
                 }
             }
 //            System.out.println("You are in the loop");
@@ -54,52 +57,175 @@ public class PerfectChange {
             n +=1;
         }
         p = cents - t;
-        System.out.println("For " + cents + " cents I give: " +
+        System.out.println("By the ADD method, for " + cents + " cents I give: " +
                 q + " quarter(s), " +
-                d + " dime(s), " +
+                d + " dime(s), "  +
                 n + " nickel(s), " +
                 p + " penn(ies). " );
 
 
     }
+    private static void makeChangeDiv(int cents){
+        int q, d, n, p , r = cents;
+        q = r / 25; // quarters to give
+        r = r - (q * 25);
+
+        d = r / 10; // dimes to give
+        r = r - (d * 10);
+
+        n = r / 5;
+        r = r - (n * 5);
+
+        p = r;
+
+        System.out.println("By the DIV method, for " + cents + " cents I give: " +
+                q + " quarter(s), " +
+                d + " dime(s), "  +
+                n + " nickel(s), " +
+                p + " penn(ies). " );
+
+
+
+
+
+
+    }
     private static void perfectChange(int cents) {
-        int qReturned = 0, dReturned = 0, nReturned = 0, pReturned = 0;
-        int t = 0;
+//        int qReturned = 0, dReturned = 0, nReturned = 0, pReturned = 0, r = cents;
 
-        while (t + 25 <= cents && qoh > 0) {
-//            if (qoh > 0) {
-                qReturned++;
-                qoh--;
-//            }
-            t += 25;
-
-        }
-        while (t + 10 <= cents && doh > 0) {
-
-            dReturned++;
-            doh--;
-
-            t += 10;
-
-        }
-        while (t + 5 <= cents && noh > 0) {
-
-            nReturned++;
-            noh--;
-
-            t += 5;
+        int q, d, n, p, r = cents;
+        if (qoh > 0 && (r/25 >= 1)) {
 
 
-        }
-        while (t + 1 <= cents && poh > 0) {
-//            if (poh > 0) {
-                pReturned++;
-                poh--;
-
-//            }
-            t++;
-
+                q = (r / 25);
+                if( q > qoh){
+                    q = qoh;
+                }
+//                q = r / 25; // quarters to give
+                r = r - (q * 25);
+                qoh = qoh - q;
+            } else {
+                q = 0;
             }
+//            q = r / 25; // quarters to give
+//            r = r - (q * 25);
+
+
+        if (doh > 0) {
+            if ((r / 10) >= 1 && (r / 10) <= doh) {
+
+
+                d = r / 10; // dimes to give
+                r = r - (d * 10);
+                doh = doh - d;
+            } else {
+                d = 0;
+            }
+        } else {
+            d = 0;
+        }
+
+        if (noh > 0) {
+            if ((r / 5) >= 1) {
+
+
+                n = (r / 5);
+                if( n > noh){
+                    n = noh;
+                }
+                r = r - (n * 5);
+                noh = noh - n;
+            } else {
+                n = 0;
+            }
+        } else {
+            n = 0;
+        }
+            if (r > 0) {
+                if (r <= poh) {
+                    p = r;
+                    poh = poh - p;
+                    r = r - p;
+                } else {
+                    qoh = q + qoh;
+                    doh = d + doh;
+                    noh = n + noh;
+                    p = 0;
+                    r = r - poh;
+
+
+                }
+            }
+            else if (r != 0) {
+                qoh = q + qoh;
+                doh = d + doh;
+                noh = n + noh;
+                p = 0;
+            }
+
+//                }
+            else {
+                p = 0;
+            }
+
+
+
+
+
+//        p = r;
+
+//        System.out.println("By the DIV method, for " + cents + " cents I give: " +
+//                q + " quarter(s), " +
+//                d + " dime(s), "  +
+//                n + " nickel(s), " +
+//                p + " penn(ies). " );
+
+//        System.out.println(t + " t " + cents + " cents");
+//        int c = 0;
+
+//        while (c + 25 <= cents && qoh > 0) {
+////            if (qoh > 0) {
+//                qReturned++;
+//                qoh--;
+//                c += 25;
+//
+////            c += 25;
+////            System.out.println(t + " t " + cents + " cents");
+//
+//
+//        }
+//         while (c + 10 <= cents && doh > 0) {
+//
+//            dReturned++;
+//            doh--;
+//
+//            c += 10;
+//
+//        }
+//        while (c + 5 <= cents && noh > 0) {
+//
+//            nReturned++;
+//            noh--;
+//
+//            c += 5;
+////            System.out.println(c);
+////            System.out.println(cents);
+//////            System.out.println(poh);
+//
+//
+//        }
+//        while (poh > 0 && c + 1 <= cents) {
+//                poh--;
+//                pReturned++;
+////                System.out.println(c);
+//                c++;
+//
+//
+//
+//
+////            c++;
+//        }
+//        c++;
 //            if {
 //                System.out.println("I could not make change for " +
 //                    cents + " I am short: " + t +
@@ -113,26 +239,31 @@ public class PerfectChange {
 //        if (cents == t) {
 
 //            System.out.println(cents + t);
-        if(cents == t) {
+        if(r == 0) {
+//            System.out.println(t);
             System.out.println("For " + cents + " cents I give: " +
-                    qReturned + " quarter(s), " +
-                    dReturned + " dime(s), " +
-                    nReturned + " nickel(s), " +
-                    pReturned + " penn(ies). " +
+                    q + " quarter(s), " +
+                    d + " dime(s), " +
+                    n + " nickel(s), " +
+                    p + " penn(ies). " +
                     "leaving \n " + qoh + " quarter(s), " +
                     doh + " dime(s), " +
                     noh + " nickel(s), " +
                     poh + " penn(ies). ");
 
         } else {
-//            System.out.println(cents + t);
+            System.out.println(q);
+            System.out.println(qoh);
+
+
+//            System.out.println(c);
             System.out.println("I could not make change for " +
-                    cents + " cents. I am short: " + (cents - t) +
+                    cents + " cents. I am short: " + r +
                     " cents.");
 
 
         }
-        t = 0;
+//        t = 0;
 
 
 //        } else {
@@ -142,6 +273,7 @@ public class PerfectChange {
 //                    " cents.");
 //        }
     }
+
 
 
     private static boolean getCoinSet () {
@@ -171,12 +303,13 @@ public class PerfectChange {
     }
     private static void allValues(boolean useSet){
 
-        for(int i = 0; i <= 100; i++ ){
+        for(int i = 1; i <= 100; i++ ){
             if (useSet){
                 perfectChange(i);
             }
             else {
                 makeChange(i);
+                makeChangeDiv(i);
             }
         }
 
